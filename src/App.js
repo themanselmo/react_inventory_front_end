b6522a90b2a2f1b6309a8e2411b5cd2f38cd2b85
max anselmo <itmanselmo@gmail.com>
2022-07-24 20:59:28 -0400

    set up basic react front end for testing rust api

--- src/App.js
+++ src/App.js
@@ -1,22 +1,63 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect }  from 'react';
 
 function App() {
+
+  const [inventories, setInventories] = useState([])
+  const [showForm, setShowForm] = useState(false)
+  const [name, setName] = useState('')
+
+  useEffect(() => {
+    fetch('http://127.0.0.1:5000/inventories')
+    .then(res => res.json())
+    .then(data =>setInventories(data))
+    .catch(error => {
+      console.log(error)
+    })
+  }, [])
+
+  const createInventory = () => {
+    const newInventory = {name: name, item_count: 0}
+    const reqBody = { 
+      method: "post",
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+    
+      //make sure to serialize your JSON body
+      body: JSON.stringify(newInventory)
+    }
+
+    fetch('http://127.0.0.1:5000/inventories', reqBody)
+    .then(res => res.json())
+    .then(res => setInventories([...inventories, res]))
+  }
+
+  const renderInventories = () => inventories.map((inventory) => (
+    <li key={inventory.id}>{inventory.name}, {inventory.item_count}</li>
+  ))
+
+  const toggleForm = () => setShowForm(!showForm)
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Add, Edit or View your Inventories!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <ul>
+          {inventories.length === 0 ? 'No Inventories to display.': renderInventories()}
+        </ul>
+        <button onClick={toggleForm}>
+          New Inventory  
+        </button>   
+        { showForm && <>
+          <input value={name} onChange={(e) => setName(e.target.value)}/>
+          <button onClick={createInventory}>Submit</button>
+        </>}  
       </header>
     </div>
   );
