82a546f1028d72bf100bd6c2a2e18d6c0bebe466
max anselmo <itmanselmo@gmail.com>
2022-07-26 23:58:10 -0400

    added rough implementation of form and state for adding items to an inventory

--- src/App.js
+++ src/App.js
@@ -6,7 +6,13 @@ function App() {
 
   const [inventories, setInventories] = useState([])
   const [showForm, setShowForm] = useState(false)
+  const [showItemForm, setShowItemForm] = useState(false)
   const [name, setName] = useState('')
+  const [itemName, setItemName] = useState('')
+  const [itemDesc, setItemDesc] = useState('')
+  const [itemCat, setItemCat] = useState('')
+  const [itemQuant, setItemQuant] = useState(null)
+  const [selectedInventoryId, setSelectedInventoryId] = useState(null)
 
   useEffect(() => {
     fetch('http://127.0.0.1:5000/inventories')
@@ -26,7 +32,6 @@ function App() {
         'Content-Type': 'application/json'
       },
     
-      //make sure to serialize your JSON body
       body: JSON.stringify(newInventory)
     }
 
@@ -35,11 +40,46 @@ function App() {
     .then(res => setInventories([...inventories, res]))
   }
 
+  const createItem = () => {
+    const newItem = {
+      name: itemName, 
+      description: itemDesc, 
+      quantity: itemQuant,
+      category: itemCat,
+      inventory_id: selectedInventoryId
+    }
+    const reqBody = { 
+      method: "post",
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+    
+      body: JSON.stringify(newItem)
+    }
+    debugger
+    fetch('http://127.0.0.1:5000/items', reqBody)
+    .then(res => res.json())
+    .then(res => console.log(res))
+  }
+
+  const updateInventory = () => {
+
+  }
+
+  const deleteInventory = () => {
+    
+  }
+
   const renderInventories = () => inventories.map((inventory) => (
-    <li key={inventory.id}>{inventory.name}, {inventory.item_count}</li>
+    <li key={inventory.id} onClick={() => toggleItemForm(inventory.id)}>{inventory.name}, {inventory.item_count}</li>
   ))
 
   const toggleForm = () => setShowForm(!showForm)
+  const toggleItemForm = (inventoryId) => {
+    setShowItemForm(!showItemForm)
+    setSelectedInventoryId(inventoryId)
+  }
 
   return (
     <div className="App">
@@ -54,10 +94,21 @@ function App() {
         <button onClick={toggleForm}>
           New Inventory  
         </button>   
-        { showForm && <>
-          <input value={name} onChange={(e) => setName(e.target.value)}/>
-          <button onClick={createInventory}>Submit</button>
-        </>}  
+        { 
+          showForm && <>
+            <input value={name} onChange={(e) => setName(e.target.value)}/>
+            <button onClick={createInventory}>Submit</button>
+          </>
+        }
+        {
+          showItemForm && <>
+            <input placeholder="name" value={itemName} onChange={(e) => setItemName(e.target.value)}/>
+            <input placeholder="desc" value={itemDesc} onChange={(e) => setItemDesc(e.target.value)}/>
+            <input placeholder="quantity" value={itemQuant} onChange={(e) => setItemQuant(e.target.value)}/>
+            <input placeholder="category" value={itemCat} onChange={(e) => setItemCat(e.target.value)}/>
+            <button onClick={createItem}>Create Item</button>
+          </>
+        }
       </header>
     </div>
   );
